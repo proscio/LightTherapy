57c7b4b32da19eca4950a18e7e58745f38070f7c
pputrevu-sys <123091224+pputrevu-sys@users.noreply.github.com>
2025-04-23 16:40:02 -0400

    Update webServer.cc

--- webServer.cc
+++ webServer.cc
@@ -5,13 +5,14 @@
 
 // Load Wi-Fi library
 #include <WiFi.h>
+#include <ESPAsyncWebServer.h>
 
 // Replace with your network credentials
 const char* ssid = "";
 const char* password = "";
 
 // Set web server port number to 80
-WiFiServer server(80);
+AsyncWebServer server(80);
 
 // Variable to store the HTTP request
 String header;
@@ -25,25 +26,13 @@ const int UVB_PWM = 12;
 const int LED_PWM = 14;
 const int MODE_BTN = 21;
 
-/*
-volatile int click = 0;
-
-void IRAM_ATTR changeMode() {
-  click = (click + 1) % 3;
-}*/
-
-// Current time
-unsigned long currentTime = millis();
-// Previous time
-unsigned long previousTime = 0; 
-// Define timeout time in milliseconds (example: 2000ms = 2s)
-const long timeoutTime = 2000;
-
 void setup() {
   Serial.begin(115200);
+  
   // Initialize the output variables as outputs
   pinMode(UVB_PWM, OUTPUT);
   pinMode(LED_PWM, OUTPUT);
+  
   // Set outputs to LOW
   digitalWrite(UVB_PWM, LOW);
   digitalWrite(LED_PWM, LOW);
@@ -56,105 +45,80 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
+  
   // Print local IP address and start web server
   Serial.println("");
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+  
+  // Serve the HTML page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    String html = "<html><body><h1>ESP32 Web Server</h1>";
+    html += "<p>UVB - State " + UVB_PWMState + "</p>";
+    html += "<p>LED - State " + LED_PWMState + "</p>";
+    
+    // Buttons that will be used for AJAX requests
+    html += "<button onclick=\"toggleUVB()\">Toggle UVB</button>";
+    html += "<button onclick=\"toggleLED()\">Toggle LED</button>";
+    
+    // AJAX JavaScript
+    html += "<script>";
+    html += "function toggleUVB() {";
+    html += "  var xhr = new XMLHttpRequest();";
+    html += "  xhr.open('GET', '/toggleUVB', true);";
+    html += "  xhr.send();";
+    html += "  xhr.onload = function() {";
+    html += "    if (xhr.status == 200) {";
+    html += "      document.location.reload();";
+    html += "    }";
+    html += "  };";
+    html += "}";
+    
+    html += "function toggleLED() {";
+    html += "  var xhr = new XMLHttpRequest();";
+    html += "  xhr.open('GET', '/toggleLED', true);";
+    html += "  xhr.send();";
+    html += "  xhr.onload = function() {";
+    html += "    if (xhr.status == 200) {";
+    html += "      document.location.reload();";
+    html += "    }";
+    html += "  };";
+    html += "}";
+    html += "</script>";
+    html += "</body></html>";
+
+    request->send(200, "text/html", html);
+  });
+
+  // Handle AJAX requests to toggle UVB
+  server.on("/toggleUVB", HTTP_GET, [](AsyncWebServerRequest *request){
+    if (UVB_PWMState == "off") {
+      UVB_PWMState = "on";
+      digitalWrite(UVB_PWM, HIGH);  // Turn UVB on
+    } else {
+      UVB_PWMState = "off";
+      digitalWrite(UVB_PWM, LOW);  // Turn UVB off
+    }
+    request->send(200, "text/plain", "UVB Toggled");
+  });
+
+  // Handle AJAX requests to toggle LED
+  server.on("/toggleLED", HTTP_GET, [](AsyncWebServerRequest *request){
+    if (LED_PWMState == "off") {
+      LED_PWMState = "on";
+      digitalWrite(LED_PWM, HIGH);  // Turn LED on
+    } else {
+      LED_PWMState = "off";
+      digitalWrite(LED_PWM, LOW);  // Turn LED off
+    }
+    request->send(200, "text/plain", "LED Toggled");
+  });
+
   server.begin();
 }
 
-void loop(){
-  WiFiClient client = server.available();   // Listen for incoming clients
-
-  if (client) {                             // If a new client connects,
-    currentTime = millis();
-    previousTime = currentTime;
-    //Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
-      currentTime = millis();
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /21/on") >= 0) {
-              Serial.println("LAMP ON");
-              UVB_PWMState = "on";
-              LED_PWMState = "on";
-              digitalWrite(UVB_PWM, HIGH);
-              digitalWrite(LED_PWM, HIGH);
-            } else if (header.indexOf("GET /21/led") >= 0) {
-              Serial.println("UVB OFF");
-              UVB_PWMState = "off";
-              LED_PWMState = "on";
-              digitalWrite(UVB_PWM, LOW);
-              digitalWrite(LED_PWM, HIGH);
-            } else if (header.indexOf("GET /21/off") >= 0) {
-              Serial.println("LAMP OFF");
-              UVB_PWMState = "off";
-              LED_PWMState = "off";
-              digitalWrite(UVB_PWM, LOW);
-              digitalWrite(LED_PWM, LOW);
-            }
-            
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #555555;}</style></head>");
-            
-            // Web Page Heading
-            client.println("<body><h1>ESP32 Web Server</h1>");
-            
-            // Display current state, and ON/OFF buttons for GPIO 26  
-            client.println("<p>UVB - State " + UVB_PWMState + "</p>");
-            client.println("<p>LED - State " + LED_PWMState + "</p>");
-            // If the UVB_PWMState is off, it displays the ON button       
-            if (UVB_PWMState=="off"  && LED_PWMState == "off") {
-              client.println("<p><a href=\"/21/on\"><button class=\"button\">TURN LAMP ON</button></a></p>");
-            } else if (UVB_PWMState=="on" && LED_PWMState=="on"){
-              client.println("<p><a href=\"/21/led\"><button class=\"button button2\">TURN UVB OFF</button></a></p>");
-            } else if(UVB_PWMState=="off" && LED_PWMState=="on"){
-              client.println("<p><a href=\"/21/off\"><button class=\"button button2\">TURN LAMP OFF</button></a></p>");
-            }
-               
-            client.println("</body></html>");
-            
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    //Serial.println("Client disconnected.");
-    Serial.println("");
-  }
+void loop() {
+  // No need to do anything in the loop, as we are using the asynchronous server
 }
+
